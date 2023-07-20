1f30c06bb47aaa7d6059bab04f97933a6560d612
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2023-07-20 18:57:44 -0300

    Ajuste na colisão com o choão

--- Assets/Characters/Player/Scripts/GroundCollision.cs
+++ Assets/Characters/Player/Scripts/GroundCollision.cs
@@ -6,23 +6,50 @@ public class GroundCollision : MonoBehaviour
 
     void Start()
     {
-        playerMove = GetComponentInParent<PlayerMove>();   
+        playerMove = GetComponent<PlayerMove>();
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (!other.gameObject.CompareTag("Ground"))
-            return;
+    // void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (!other.gameObject.CompareTag("Ground"))
+    //         return;
 
-        playerMove.StopJump();
-        playerMove.SetFall(false);
+    //     playerMove.StopJump();
+    //     playerMove.SetFall(false);
+    // }
+
+    // void OnCollisionExit2D(Collision2D other)
+    // {
+    //     if (!other.gameObject.CompareTag("Ground"))
+    //         return;
+
+    //     playerMove.SetFall(true);
+    // }
+
+    public Transform[] groundChecks;
+    public LayerMask groundLayer;
+
+    bool isGrounded = false;
+
+    private void FixedUpdate()
+    {
+        // Verifica se algum dos pontos groundChecks está tocando o chão (Layer "Ground")
+        isGrounded = CheckGrounded();
+
+        if (isGrounded)
+            playerMove.StopJump();
+
+        playerMove.SetFall(!isGrounded);
     }
 
-    void OnCollisionExit2D(Collision2D other)
+    bool CheckGrounded()
     {
-        if (!other.gameObject.CompareTag("Ground"))
-            return;
-
-        playerMove.SetFall(true);
+        foreach (Transform groundCheck in groundChecks)
+        {
+            var hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.2f);
+            if (hit.collider != null && hit.collider.gameObject.CompareTag("Ground"))
+                return true;
+        }
+        return false;
     }
 }
